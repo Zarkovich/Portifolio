6fd9cc6ae1e8ea55c8e937267f41e1e09585fd0f
Gustavo Vieira <gustavovieirazu@gmail.com>
2022-08-02 23:56:16 -0300

    Feature: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import "./Styles/App.scss";
 import NavBar from "./Components/NavBar/NavBar";
+import ilustraton from "./assets/Img/imagemSobrefundo.svg";
 
 const App = () => {
   return (
     <div className='App'>
       <NavBar />
+      <div className='About'>
+        <div className='About__Description'>
+          <p>DESENVOLVEDOR FULL-STACK</p>
+          <h1>Gustavo Vieira</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Necessitatibus magnam est eum soluta quisquam. Quibusdam,
+            consectetur! Consequatur architecto est veritatis.
+          </p>
+          <button>BAIXAR CURRÍCULO</button>
+        </div>
+        <div className='About__Image'>
+          <img src={ilustraton} alt='imagem' />
+        </div>
+      </div>
     </div>
   );
 };
